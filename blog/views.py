ba3222ca4385914ab9a09967317dc94def711b05
Anael Varela <ken_cond@yaoo.com>
2022-11-17 21:30:27 -0600

    Agrgando Blog

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .forms import PostCreateForm
+
+class BlogListView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            
+        }
+        return render(request, 'blog_list.html', context)
+    
+class BlogCreateView(View):
+    def get(self, request, *args, **kwags):
+        context = {
+            
+        }
+        return render(request, 'blog_create.html', context)
+    
+    def post(self, request, *args, **kwags):
+        context = {
+            
+        }
+        return render(request, 'blog_create.html', context)
